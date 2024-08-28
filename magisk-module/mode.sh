222abf080f40a575d882d852ea071101b33e88dc
zata1118 <126246870+zata1118@users.noreply.github.com>
2024-08-28 15:08:01 +0800

    Update mode.sh

--- magisk-module/mode.sh
+++ magisk-module/mode.sh
@@ -3,7 +3,7 @@
 # 1 - read-write default
 # 2 - read-only locked (cannot remount as read-write)
 # You can set to 2 after modify system partititons to avoid detection
-export OVERLAY_MODE=0
+export OVERLAY_MODE=1
 
 # Set to true to mount overlayfs on subdirectories of /system /vendor /system_ext /product,...
 # instead of on /system /vendor /system_ext /product,...
